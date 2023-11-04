dc428f39ece8e2659ad8d80398609693ae256511
Meksich <maks10031003@gmail.com>
2023-11-04 18:46:20 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Chocolate_Robo_Android_App_UI"
+include(":app")
+ 
